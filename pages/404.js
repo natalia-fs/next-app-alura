9482c6cb2b08705b407a25a6ab525ec215079727
Natalia <natalia-321s@outlook.com>
2022-06-13 00:02:22 -0300

    Aula 1

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Link from '../src/components/Link'
+export default function Page404(){
+  return(
+    <div>
+      <h1>404 - Página não encontrada</h1>
+      <Link href="/" passHref>Ir para a Home</Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+import Link from '../src/components/Link'
+export default function FAQPage(){
+  return(
+    <div>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/" passHref>Ir para a Home</Link>
+    </div>
+  )
+}
