129ddcbc94ec1ac4866d0484af9278eb1d3332e8
Natalia <natalia-321s@outlook.com>
2022-06-16 15:19:56 -0300

    Aula 3 - Estilizando o projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import GlobalStyle from '../src/theme/GlobalStyle'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Link from '../src/components/Link'
 
+function Title({children, as}){
+  const Tag = as;
+  return (
+    <>
+      <Tag>
+        {children}
+      </Tag>
+      <style>{`
+        ${Tag}{
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  )
+}
+
 export default function Home() {
   return (
     <div>
-      <h1>Alura Cases - Home Page</h1>
+      <Title as="h1">Alura Cases - Home Page</Title>
       <Link href="/faq">Ir para a página do FAQ</Link>
     </div>
   )
