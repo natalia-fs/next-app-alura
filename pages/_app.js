eaf69e2bfa6b4f36741f73fba0e7ca23d0a9498b
Natalia <natalia-321s@outlook.com>
2022-07-31 23:30:38 -0300

    Corrigindo falha no build causada no _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,24 +5,25 @@ import GlobalStyle from '../src/theme/GlobalStyle'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4Y2B58E5VP"></Script>
+      <Script
+        id="google_analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-4Y2B58E5VP');
+          `
+        }}
+      />
       <Head>
 
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4Y2B58E5VP"></Script>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-    
-              gtag('config', 'G-4Y2B58E5VP');
-            `
-          }}
-        />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
       </Head>
       <GlobalStyle/>
